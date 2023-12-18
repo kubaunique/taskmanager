37baf439de29dbe417cb270c3ef895d1cc614ae4
kubaunique <121674973+kubaunique@users.noreply.github.com>
2023-12-18 15:00:25 +0600

    Delete kubanychTaskManager directory

--- kubanychTaskManager/src/main/java/com/example/deploy_app_mid_2023_2nd_sem/repositories/itemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.deploy_app_mid_2023_2nd_sem.repositories;
-
-
-import com.example.deploy_app_mid_2023_2nd_sem.models.TodoItem;
-import org.springframework.data.repository.CrudRepository;
-
-public interface itemRepository extends CrudRepository<TodoItem, Long> {
-}
